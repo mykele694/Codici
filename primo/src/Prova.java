e7ea4394758dfe107ec7cf86dff7cd8a2dd894c7
mykele694 <michele.d.94@hotmail.it>
2023-02-01 14:18:32 +0100

    Code

---
+++ primo/src/Prova.java
@@ -0,0 +1,13 @@
+public class Prova {
+
+    public static void main(String[] args){
+
+        int x=12;  //Variabile
+        final int y=5; //Costante
+
+        System.out.println("Hello World"); //sout per la shortcut
+
+
+    }
+
+}
